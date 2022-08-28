b249b051f4d53f69bdf15acf55f1c627e0aadc84
norrisaboagye <norrisboat@gmail.com>
2022-08-28 07:44:00 +0000

    Initial Commit

---
+++ SleepySwiftUI/SleepySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepySwiftUIApp.swift
+//  SleepySwiftUI
+//
+//  Created by Norris Aboagye on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SleepySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
